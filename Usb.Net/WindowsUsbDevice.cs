a071f23b4b3fd0711d4262c7f49f1c4f905add79
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-12-16 17:14:13 +1100

    Move some Usb specific stuff in here.

---
+++ Usb.Net/WindowsUsbDevice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Hid.Net
+{
+    public class WindowsUsbDevice : WindowsDeviceBase
+    {
+        #region Public Methods
+        public override ushort WriteBufferSize { get; }
+        public override ushort ReadBufferSize { get; }   
+        #endregion
+
+        #region Constructor
+        public WindowsUsbDevice(string deviceId, ushort writeBufferSzie, ushort readBufferSize) : base(deviceId)
+        {
+            WriteBufferSize = writeBufferSzie;
+            ReadBufferSize = readBufferSize;
+        }
+        #endregion
+    }
+}
