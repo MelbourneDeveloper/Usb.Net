c873398a69bbd19674b1ea87c4d5e85b8414d01a
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-12-18 19:46:34 +1100

    Add a sample

---
+++ Usb.Net.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Usb.Net.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
