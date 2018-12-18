ba4cceb1aacee008ce43ad0f17a7e52458afdfd3
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-12-18 19:57:22 +1100

    Write a test for keepkey

--- Usb.Net.Sample/Program.cs
+++ Usb.Net.Sample/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Device.Net;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Usb.Net.Sample
 {
@@ -6,7 +9,30 @@ namespace Usb.Net.Sample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Go();
+
+            Console.ReadLine();
+        }
+
+        private static async Task Go()
+        {
+            var devices = WindowsDeviceBase.GetConnectedDeviceInformations(WindowsDeviceConstants.GUID_DEVINTERFACE_USB_DEVICE);
+
+            var device = devices.FirstOrDefault(d => d.DevicePath.ToLower().Contains("2b24"));
+
+            var windowsUsbDevice = new WindowsUsbDevice(device.DevicePath, 64, 64);
+
+            await windowsUsbDevice.InitializeAsync();
+
+            var buffer = new byte[64];
+            buffer[0] = 0x3f;
+            buffer[1] = 0x23;
+            buffer[2] = 0x23;
+
+            await windowsUsbDevice.WriteAsync(buffer);
+
+            var asdasd = await windowsUsbDevice.ReadAsync();
         }
     }
 }
